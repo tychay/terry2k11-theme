88422dd751353d752814c1639c24a93c34a209d9
terry chay <tychay@php.net>
2015-05-07 16:29:42 -0700

    Support for terry's commentary tooltip
    Signed-off-by: terry chay <tychay@php.net>

--- functions.php
+++ functions.php
@@ -8,5 +8,14 @@ function theme_enqueue_styles() {
     );
 }
 
+if ( function_exists( 'fml_register_sizes' ) ) {
+	fml_register_sizes(
+		'theme-medium-sizes',
+		'(max-width: 30em) 100vw, (max-width: 50em) 50vw, calc(33vw - 100px)',
+		array( 'medium', 'Medium')
+	);
+}
 // add picturefill cdn support
 add_filter( 'picturefill_wp_use_cdn', '__return_true' );
+// eliminate commentary's default stylesheet
+add_filter( 'tccomment_add_default_css', '__return_false' );
